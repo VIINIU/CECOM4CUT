0af97bac6a6e58a277a6737cdef21a784cf8f050
yymin1022 <yymin1022@gmail.com>
2024-08-12 23:51:15 +0900

    Update print_msg.sh : Added Exception for Argument

--- CECOM4CUT_Scripts/print_msg.sh
+++ CECOM4CUT_Scripts/print_msg.sh
@@ -2,6 +2,12 @@
 
 PRINT_HANDLE=0x2c
 
+if [ $# -ne 1 ]; then
+    echo "Error: Message Parameter is Needed"
+    exit 1
+fi
+
+
 PRINT_STRING=$1
 HEX_STRING=$(echo -n "$PRINT_STRING" | od -A n -t x1 | tr -d ' \n')
 
