0cf08c64cfe8f77b36cb542a209fa566a2db1dbf
yymin1022 <yymin1022@gmail.com>
2024-08-12 23:56:40 +0900

    Update print_bmp.sh : Added File Hex Converting Logic

--- CECOM4CUT_Scripts/print_bmp.sh
+++ CECOM4CUT_Scripts/print_bmp.sh
@@ -8,5 +8,7 @@ if [ $# -ne 1 ]; then
 fi
 
 PRINT_FILE_NAME=$1
+HEX_FILE_DATA=$(xxd -p -c 256 "$PRINT_FILE_NAME" | tr -d '\n')
 
+gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=$HEX_FILE_DATA
 gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=0a0a0a0a0a
