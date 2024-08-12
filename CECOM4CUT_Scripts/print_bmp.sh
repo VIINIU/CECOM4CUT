1ae1e9a47b1e0df6c64080054c4a5b5f8e6eab60
yymin1022 <yymin1022@gmail.com>
2024-08-13 00:10:56 +0900

    Update print_bmp.sh : Added File Chunk Printing Logic

--- CECOM4CUT_Scripts/print_bmp.sh
+++ CECOM4CUT_Scripts/print_bmp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 PRINT_HANDLE=0x2c
 
@@ -10,5 +10,13 @@ fi
 PRINT_FILE_NAME=$1
 HEX_FILE_DATA=$(xxd -p -c 256 "$PRINT_FILE_NAME" | tr -d '\n')
 
-gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=$HEX_FILE_DATA
+HEX_FILE_LEN=${#HEX_FILE_DATA}
+GATT_CHUNK_SIZE=250
+
+for (( i=0; i<$HEX_FILE_LEN; i+=$GATT_CHUNK_SIZE )); do
+	TMP_SEND_DATA=${HEX_FILE_DATA:i:GATT_CHUNK_SIZE}
+	gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=$TMP_SEND_DATA
+	sleep 1
+done
+
 gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=0a0a0a0a0a
