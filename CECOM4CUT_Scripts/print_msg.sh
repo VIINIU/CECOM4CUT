e88f0a2bcab3e3b96817d8a785b6a3a508019bb8
yymin1022 <yymin1022@gmail.com>
2024-08-12 23:52:28 +0900

    Update print_msg.sh : Added 5 LFs for Print

--- CECOM4CUT_Scripts/print_msg.sh
+++ CECOM4CUT_Scripts/print_msg.sh
@@ -12,4 +12,4 @@ PRINT_STRING=$1
 HEX_STRING=$(echo -n "$PRINT_STRING" | od -A n -t x1 | tr -d ' \n')
 
 gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=$HEX_STRING
-gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=0a
+gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=0a0a0a0a0a
