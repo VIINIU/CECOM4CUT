7ac2e62b840c41866f719748d95e256c5f474d11
yymin1022 <yymin1022@gmail.com>
2024-08-13 22:23:42 +0900

    Update print_bmp.sh : Adjust Printing Logic

--- CECOM4CUT_Scripts/print_bmp.sh
+++ CECOM4CUT_Scripts/print_bmp.sh
@@ -7,8 +7,8 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-IMG_HEIGHT=100
-IMG_WIDTH=200
+IMG_HEIGHT=255
+IMG_WIDTH=320
 
 ucTemp0="1d"
 ucTemp1=$(printf "%x" "'v")
@@ -21,17 +21,16 @@ ucTemp7=$(printf "%02x" $((IMG_HEIGHT >> 8)))
 BMP_HEADER="$ucTemp0$ucTemp1$ucTemp2$ucTemp3$ucTemp4$ucTemp5$ucTemp6$ucTemp7"
 
 gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=$BMP_HEADER
+gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=00000000000000000000
 
 PRINT_FILE_NAME=$1
-HEX_FILE_DATA=$(xxd -p -c 256 "$PRINT_FILE_NAME" | tr -d '\n')
+HEX_FILE_DATA=$(xxd -p "$PRINT_FILE_NAME" | tr -d '\n')
 
 HEX_FILE_LEN=${#HEX_FILE_DATA}
-GATT_CHUNK_SIZE=250
 
-for (( i=0; i<$HEX_FILE_LEN; i+=$GATT_CHUNK_SIZE )); do
-	TMP_SEND_DATA=${HEX_FILE_DATA:i:GATT_CHUNK_SIZE}
+for (( i=0; i<$HEX_FILE_LEN; i+=$IMG_WIDTH )); do
+	TMP_SEND_DATA=${HEX_FILE_DATA:i:IMG_WIDTH}
 	gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=$TMP_SEND_DATA
-	sleep 1
 done
 
 gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=0a0a0a0a0a
