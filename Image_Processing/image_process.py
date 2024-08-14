39fa4bb0b3f8b17f15113cadb41d5f1fc291d655
yymin1022 <yymin1022@gmail.com>
2024-08-15 03:18:58 +0900

    Update image_process.py : Added Filename from Parameter

--- Image_Processing/image_process.py
+++ Image_Processing/image_process.py
@@ -1,18 +1,26 @@
 from PIL import Image
+import sys
 
 def convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path):
 	target_width = 320
 
 	img = Image.open(input_jpg_path)
-	img = img.convert('L')
+	img = img.convert('1')
 
 	orig_width, orig_height = img.size
 	target_height = int((target_width / orig_width) * orig_height)
 
 	img = img.resize((target_width, target_height))
-	img = img.point(lambda x: 0 if x < 128 else 255, '1')
+	img = img.point(lambda x: 255 if x < 128 else 0, '1')
 	img.save(output_bmp_path, format='BMP')
 
-input_jpg_path = 'input.jpg'
-output_bmp_path = 'output.bmp'
-convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path)
+def main(argv):
+    if len(argv) != 3:
+        print("usage: python3 image_process.py <original_jpg_filename> <result_bmp_filename>")
+        return
+
+    convert_jpg_to_1bit_bmp(argv[1], argv[2])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
