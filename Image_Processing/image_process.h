9c092aa2101c2416c6860b0b5a00396af0cdccd5
yymin1022 <yymin1022@gmail.com>
2024-08-14 22:06:39 +0900

    Update image_process.h : Added JPEG Function Definition

--- Image_Processing/image_process.h
+++ Image_Processing/image_process.h
@@ -14,4 +14,7 @@
 struct jpeg_decompress_struct   cinfo;
 struct jpeg_error_mgr           jerr;
 
+void			write_1bit_bmp(const char *filename, unsigned char *image_buffer, int width, int height);
+unsigned char	*resize_image(unsigned char *image_buffer, int orig_width, int orig_height, int new_width, int *new_height);
+
 #endif
