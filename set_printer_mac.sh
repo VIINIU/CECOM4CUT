2b2b6e64caa5d22cc6ca14255c1796f524c93130
yymin1022 <yymin1022@gmail.com>
2024-08-15 03:29:21 +0900

    Update set_printer_mac.sh : Added Exception for Not Found

--- set_printer_mac.sh
+++ set_printer_mac.sh
@@ -3,5 +3,10 @@ echo "Finding Printer Device..."
 
 MAC_ADDR=$(hcitool scan | grep PT-210 | awk '{print $1}')
 
+if [ -z "$MAC_ADDR" ]; then
+	echo "Printer Not Found!"
+	exit 1
+fi
+
 echo "Found Printer! Address is $MAC_ADDR"
 export PRINTER_MAC=$MAC_ADDR
