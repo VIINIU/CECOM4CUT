f5643009246da8cbbc4f0ded4179f35b5c97a546
yymin1022 <yymin1022@gmail.com>
2024-08-15 03:15:33 +0900

    Update camera.py : Added Filename from Parameter

--- Image_Capture/camera.py
+++ Image_Capture/camera.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-import time
 from picamera2 import Picamera2, Preview
+import sys
+import time
 
-def capture_image():
+def capture_image(filename):
     picam2 = Picamera2()
     picam2.start()
     time.sleep(2)
 
-    picam2.capture_file("test.jpg")
+    picam2.capture_file(filename)
     picam2.close()
 
+def main(argv):
+    if len(argv) != 2:
+        print("usage: python3 camera.py <filename>")
+        return
+    
+    capture_image(argv[1])
+
 if __name__ == "__main__":
-    capture_image()
+    main(sys.argv)
