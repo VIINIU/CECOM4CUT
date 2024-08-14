22238eaffa2b1df787be21906c0f2b0066208d7e
yymin1022 <yymin1022@gmail.com>
2024-08-15 03:22:15 +0900

    Update process.py : Set result height from Parameter

--- Image_Processing/process.py
+++ Image_Processing/process.py
@@ -1,23 +1,20 @@
 from PIL import Image
 import sys
 
-def convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path, target_width):
+def convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path, target_width, target_height):
 	img = Image.open(input_jpg_path)
 	img = img.convert('1')
-
-	orig_width, orig_height = img.size
-	target_height = int((target_width / orig_width) * orig_height)
 
 	img = img.resize((target_width, target_height))
 	img = img.point(lambda x: 255 if x < 128 else 0, '1')
 	img.save(output_bmp_path, format='BMP')
 
 def main(argv):
-    if len(argv) != 4:
-        print("usage: python3 process.py <original_jpg_filename> <result_bmp_filename> <result_width>")
+    if len(argv) != 5:
+        print("usage: python3 process.py <original_jpg_filename> <result_bmp_filename> <result_width> <result_height>")
         return
 
-    convert_jpg_to_1bit_bmp(argv[1], argv[2], int(argv[3]))
+    convert_jpg_to_1bit_bmp(argv[1], argv[2], int(argv[3]), int(argv[4]))
 
 
 if __name__ == "__main__":
