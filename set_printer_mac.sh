18a1daa2859d277ca24ef64b7304dd6fe13dc2fe
yymin1022 <yymin1022@gmail.com>
2024-08-15 03:27:23 +0900

    Update set_printer_mac : Added BLE Scanning

--- set_printer_mac.sh
+++ set_printer_mac.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "Hello, World!"
+echo "Finding Printer Device..."
+
+MAC_ADDR=$(hcitool scan | grep PT-210 | awk '{print $1}')
+
+echo "Found Printer! Address is $MAC_ADDR"
+export PRINTER_MAC=$MAC_ADDR
