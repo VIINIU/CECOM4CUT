adbbd6e35f3f1b0eb6f570856061f61023d1e57b
yymin1022 <yymin1022@gmail.com>
2024-08-15 03:10:45 +0900

    Update camera.py : Removed Unnecessary Preview

--- Image_Capture/camera.py
+++ Image_Capture/camera.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-
-# Capture a JPEG while still running in the preview mode. When you
-# capture to a file, the return value is the metadata for that image.
-
 import time
-
 from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
-
-preview_config = picam2.create_preview_configuration(main={"size": (800, 600)})
-picam2.configure(preview_config)
 
 picam2.start()
 time.sleep(2)
