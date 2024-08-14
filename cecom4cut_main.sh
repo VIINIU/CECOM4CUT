fbdc7eea7330c2ee9cf59c43f4816cf1125c4ef8
yymin1022 <yymin1022@gmail.com>
2024-08-15 03:38:10 +0900

    Update cecom4cut_main.sh : Added Basic Flow

--- cecom4cut_main.sh
+++ cecom4cut_main.sh
@@ -1,2 +1,17 @@
 #!/bin/bash
-echo "Hello, World!"
+
+IMAGE_FILENAME=test
+IMAGE_HEIGHT=240
+IMAGE_WIDTH=320
+
+echo "Capturing Image..."
+python3 Image_Capture/camera.py "$IMAGE_FILENAME.jpg" >> /dev/null
+echo "Capture Image Done..!"
+
+echo "Processing Image..."
+python3 Image_Processing/process.py "$IMAGE_FILENAME.jpg" "$IMAGE_FILENAME.bmp" $IMAGE_WIDTH $IMAGE_HEIGHT
+echo "Process Image Done..!"
+
+echo "Printing Image..."
+bash Print_Scripts/print_bmp.sh "$IMAGE_FILENAME.bmp" $IMAGE_HEIGHT $IMAGE_WIDTH
+echo "Print Image Done..!"
