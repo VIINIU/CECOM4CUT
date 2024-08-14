cc79a2483aa07c89004d049e1cfcf2ab17d62949
yymin1022 <yymin1022@gmail.com>
2024-08-15 04:05:36 +0900

    Update print_feed.sh : Added Feeding Script

--- Print_Scripts/print_feed.sh
+++ Print_Scripts/print_feed.sh
@@ -7,8 +7,7 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 
-PRINT_STRING=$1
-HEX_STRING=$(echo -n "$PRINT_STRING" | od -A n -t x1 | tr -d ' \n')
-
-gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=$HEX_STRING
-gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=0a0a0a0a0a
+FEED_LINE=$1
+for i in $(seq 1 $FEED_LINE); do
+	gatttool -b $PRINTER_MAC --char-write-req --handle=$PRINT_HANDLE --value=0a
+done
