1898130640011b83b4c210af018667c6a24eb41c
yymin1022 <yymin1022@gmail.com>
2024-08-14 22:40:29 +0900

    Update image_process.py : Added BMP Convering Logic

--- Image_Processing/image_process.py
+++ Image_Processing/image_process.py
@@ -1,1 +1,11 @@
-print("hello, World!")
+from PIL import Image
+
+def convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path):
+    img = Image.open(input_jpg_path)
+    img = img.convert('L')
+    img = img.point(lambda x: 0 if x < 128 else 255, '1')
+    img.save(output_bmp_path, format='BMP')
+
+input_jpg_path = 'input.jpg'
+output_bmp_path = 'output.bmp'
+convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path)
