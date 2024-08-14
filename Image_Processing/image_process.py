5f071a505bee61317df010c079229fa32aaa8100
yymin1022 <yymin1022@gmail.com>
2024-08-14 22:45:43 +0900

    Update image_process.py : Added Image Resizing Logic

--- Image_Processing/image_process.py
+++ Image_Processing/image_process.py
@@ -1,8 +1,15 @@
 from PIL import Image
 
 def convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path):
+	target_width = 320
+
 	img = Image.open(input_jpg_path)
 	img = img.convert('L')
+
+	orig_width, orig_height = img.size
+	target_height = int((target_width / orig_width) * orig_height)
+
+	img = img.resize((target_width, target_height))
 	img = img.point(lambda x: 0 if x < 128 else 255, '1')
 	img.save(output_bmp_path, format='BMP')
 
