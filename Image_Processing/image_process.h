f71a5828cd8fa61bb5e836e12f4724589efb2667
yymin1022 <yymin1022@gmail.com>
2024-08-14 22:00:11 +0900

    Update image_process.h : Defined JPEG Options

--- Image_Processing/image_process.h
+++ Image_Processing/image_process.h
@@ -1,8 +1,13 @@
 #ifndef CECOM4CUT_IMAGE_PROCESS_H
 # define CECOM4CUT_IMAGE_PROCESS_H
 
+# include <jpeglib.h>
+
 # define FALSE 0
 # define TRUE 1
 # define RESULT_WIDTH 320
+
+struct jpeg_decompress_struct   cinfo;
+struct jpeg_error_mgr           jerr;
 
 #endif
