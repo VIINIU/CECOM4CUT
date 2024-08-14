0851d164f33e96a15ad494e8b526e8871d8883be
yymin1022 <yymin1022@gmail.com>
2024-08-14 20:27:54 +0900

    Update Image_Processing : Added Argument Logic

--- Image_Processing/main.c
+++ Image_Processing/main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int	main()
+int main(int argc, char **argv)
 {
-	printf("Hello, World!\n");
+    char    *org_file_name;
+    char    *res_file_name;
+
+    if (argc != 3)
+    {
+        fprintf(stderr, "usage: ./image_process <original file> <result file>");
+        return (1);
+    }
+    org_file_name = strdup(argv[1]);
+    res_file_name = strdup(argv[2]);
+	printf("Original Filename: %s / Result Filename: %s\n", org_file_name, res_file_name);
+    free(org_file_name);
+    free(res_file_name);
 	return (0);
 }
