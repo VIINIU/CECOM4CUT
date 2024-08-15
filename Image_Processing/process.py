79ee892b590fae5d7150e3287a93ddef1404d7f3
yymin1022 <yymin1022@gmail.com>
2024-08-16 03:22:47 +0900

    Update Image_Processing : Applied Floyd Dithering for Image

--- Image_Processing/process.py
+++ Image_Processing/process.py
@@ -3,7 +3,7 @@ import sys
 
 def convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path, target_width, target_height):
 	img = Image.open(input_jpg_path)
-	img = img.convert('1')
+	img = img.convert('1', dither=Image.FLOYDSTEINBERG)
 
 	img = img.resize((target_width, target_height))
 	img = img.point(lambda x: 255 if x < 128 else 0, '1')
