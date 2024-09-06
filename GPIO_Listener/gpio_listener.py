cd6599172df52c4d7f1b73d8856c9fdb6abb2395
yymin1022 <yymin1022@gmail.com>
2024-09-06 20:44:51 +0900

    Update GPIO_Listener :Cleaned UP

--- GPIO_Listener/gpio_listener.py
+++ GPIO_Listener/gpio_listener.py
@@ -9,9 +9,7 @@ GPIO.setup(24, GPIO.IN)
 
 while True:
 	if GPIO.input(23) == GPIO.HIGH:
-		print("23 HIGH")
 		subprocess.run(["sh", "/home/yong/Projects/CECOM4CUT/cecom4cut_main.sh"])
 	if GPIO.input(24) == GPIO.HIGH:
-		print("24 HIGH")
 		subprocess.run(["sh", "/home/yong/Projects/CECOM4CUT/cecom4cut_reprint.sh"])
 	time.sleep(1)
