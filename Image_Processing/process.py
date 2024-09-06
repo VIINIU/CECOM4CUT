e792809f8078339a5803c6a2e0c837bb47fbf623
yymin1022 <yymin1022@gmail.com>
2024-09-06 20:22:29 +0900

    Update Image_Processing : Added Border Printing

--- Image_Processing/process.py
+++ Image_Processing/process.py
@@ -4,10 +4,13 @@ import sys
 def convert_jpg_to_1bit_bmp(input_jpg_path, output_bmp_path, target_width, target_height):
 	img = Image.open(input_jpg_path)
 	img = img.resize((target_width, target_height))
-	img = img.transpose(Image.FLIP_TOP_BOTTOM)
-	img = img.convert('1', dither=Image.FLOYDSTEINBERG)
-	img = img.point(lambda x: 255 if x < 128 else 0, '1')
-	img.save(output_bmp_path, format='BMP')
+
+	img_bg = Image.new("RGB", (target_width, target_height + 5), "white")
+	img_bg.paste(img, (0, 0))
+	img_bg = img_bg.convert("1", dither=Image.FLOYDSTEINBERG)
+	img_bg = img_bg.point(lambda x: 255 if x < 128 else 0, "1")
+	img_bg = img_bg.transpose(Image.FLIP_TOP_BOTTOM)
+	img_bg.save(output_bmp_path, format="BMP")
 
 def main(argv):
     if len(argv) != 5:
