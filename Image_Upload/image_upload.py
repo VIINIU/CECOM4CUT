cf4d2fc8e0d35045ca5afc2a20eb731e80d7c748
yymin1022 <yymin1022@gmail.com>
2024-09-06 17:16:26 +0900

    Update Image_Upload : Added File Uploading Logic

--- Image_Upload/image_upload.py
+++ Image_Upload/image_upload.py
@@ -1,7 +1,23 @@
+import base64
+import requests
 import sys
 
-def upload_file(filename):
-	print("Upload {filename}")
+def upload_file(file_name):
+	with open(file_name, "rb") as file:
+		file_buffer = base64.b64encode(file.read()).decode("utf-8")
+
+	upload_data = {
+		"file": file_buffer,
+		"filename": file_name,
+	}
+
+	upload_url = "https://4cut.cecom.dev/upload"
+	upload_header = {
+		"Content-Type": "application/json"
+	}
+
+	upload_response = requests.post(upload_url, headers=upload_header, json=upload_data)
+	print(upload_response)
 
 def main(argv):
 	if len(argv) != 2:
