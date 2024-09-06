50e885cd09a179685e842a730b5154771c156e47
yymin1022 <yymin1022@gmail.com>
2024-09-06 16:51:17 +0900

    Update Image_Upload : Added Function Calls

--- Image_Upload/image_upload.py
+++ Image_Upload/image_upload.py
@@ -1,1 +1,14 @@
-print("Hello, World!")
+import sys
+
+def upload_file(filename):
+	print("Upload {filename}")
+
+def main(argv):
+	if len(argv) != 2:
+		print("usage: python3 image_upload.py <filename>")
+		return
+
+	upload_file(argv[1])
+
+if __name__ == "__main__":
+	main(sys.argv)
