708b3a52f0540d8991ba022e0bf51f28cfa79d99
yymin1022 <yymin1022@gmail.com>
2024-09-06 18:35:58 +0900

    Update Image_QR : Added QR Generating Logic

--- Image_QR/qr_generator.py
+++ Image_QR/qr_generator.py
@@ -1,1 +1,13 @@
-print("Hello, World!")
+from PIL import Image
+import qrcode
+import sys
+
+def qr_gen(filename):
+	img = qrcode.make(f"https://4cut.cecom.dev/download/{filename}")
+	img.save(f"{filename}.qr.bmp")
+
+def main(argv):
+	qr_gen(argv[1])
+
+if __name__ == "__main__":
+	main(sys.argv)
