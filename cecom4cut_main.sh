4aa52a2403cba890a3ac0f0194350d7a7f37c969
yymin1022 <yymin1022@gmail.com>
2024-09-06 21:35:05 +0900

    Update cecom4cut_main.sh : Removed Delay

--- cecom4cut_main.sh
+++ cecom4cut_main.sh
@@ -28,11 +28,8 @@ echo "Process Image Done..!"
 
 echo "Printing Image..."
 bash Print_Scripts/print_bmp.sh "resources/frame_h_rev.bmp" 40 $IMAGE_WIDTH
-sleep 0.1
 bash Print_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.bmp" $((IMAGE_HEIGHT + 5)) $IMAGE_WIDTH
-sleep 0.1
 bash Print_Scripts/print_bmp.sh "resources/frame_f_rev.bmp" 255 $IMAGE_WIDTH
-sleep 0.1
 bash Print_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.qr.bmp" $((QR_HEIGHT + 5)) $QR_WIDTH
 sleep 0.1
 bash Print_Scripts/print_feed.sh 5
