6b3d3d28633a68fdddb5abe6135a2720a9431df1
yymin1022 <yymin1022@gmail.com>
2024-09-06 20:44:06 +0900

    Update cecom4cut_reprint.sh : Added Reprint Logic

--- cecom4cut_reprint.sh
+++ cecom4cut_reprint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
+LAST_FILE_DATE=$(ls result/ | grep ".bmp" | grep -v "qr" | tac | head -n 1 | sed 's/\.bmp//')
 
-IMAGE_FILENAME=$CURRENT_DATE
+IMAGE_FILENAME=$LAST_FILE_DATE
 IMAGE_HEIGHT=240
 IMAGE_WIDTH=320
 QR_HEIGHT=100
@@ -15,16 +15,6 @@ fi
 
 sleep 1
 
-echo "Capturing Image..."
-python3 Image_Capture/camera.py "result/$IMAGE_FILENAME.jpg" >> /dev/null
-echo "Capture Image Done..!"
-
-echo "Processing Image..."
-python3 Image_Processing/process.py "result/$IMAGE_FILENAME.jpg" "result/$IMAGE_FILENAME.bmp" $IMAGE_WIDTH $IMAGE_HEIGHT
-python3 Image_QR/qr_generator.py "result/$IMAGE_FILENAME"
-python3 Image_Processing/process.py "result/$IMAGE_FILENAME.qr.bmp" "result/$IMAGE_FILENAME.qr.bmp" $QR_WIDTH $QR_HEIGHT
-echo "Process Image Done..!"
-
 echo "Printing Image..."
 bash Print_Scripts/print_bmp.sh "resources/frame_h_rev.bmp" 40 $IMAGE_WIDTH
 sleep 0.1
@@ -36,7 +26,3 @@ bash Print_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.qr.bmp" $((QR_HEIGHT + 5
 sleep 0.1
 bash Print_Scripts/print_feed.sh 5
 echo "Print Image Done..!"
-
-echo "Uploading Image..."
-python3 Image_Upload/image_upload.py "result/$IMAGE_FILENAME.jpg"
-echo "Upload Image Done..!"
