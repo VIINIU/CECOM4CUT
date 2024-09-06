9f82b6668d36737fa9a695f3b91f05623fcb7499
yymin1022 <yymin1022@gmail.com>
2024-09-06 21:04:30 +0900

    Update gpio_led.py : Added Blink Logic

--- GPIO_Listener/gpio_led.py
+++ GPIO_Listener/gpio_led.py
@@ -1,1 +1,10 @@
-print("Hello, World!")
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(25, GPIO.OUT)
+
+GPIO.output(25, GPIO.HIGH)
+time.sleep(0.3)
+GPIO.output(25, GPIO.LOW)
+time.sleep(0.3)
