cdac02cd58bc33b59cb08bd0449eb760c64ed1d4
yymin1022 <yymin1022@gmail.com>
2024-12-28 16:28:34 +0900

    Update cecom4cut_installer : Added Description Messages for Each Step

--- cecom4cut_installer.sh
+++ cecom4cut_installer.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
+echo "Installing CECOM4CUT Service..."
+echo ""
+echo ""
+
+echo "Intalling Project Dependencies..."
+echo ""
+
 sudo apt update && sudo apt install -y python3 bluez
 if [ $? -ne 0 ]; then
     echo "Error: Failed to install dependency packages."
     exit 1
 fi
+
+echo ""
+echo "Installed PRoject Dependencies"
+echo ""
+
+echo ""
+echo "Finding Printer and Registering MAC Address..."
+echo ""
 
 bash Printer_Scripts/set_printer_mac.sh
 if [ $? -ne 0 ]; then
@@ -11,8 +26,23 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
+echo ""
+echo "Found Printer and Registered MAC Address"
+echo ""
+
+
+echo ""
+echo "Installing CECOM4CUT Service Daemon..."
+echo ""
+
 sh Systemd_scripts/systemd_installer.sh
 if [ $? -ne 0 ]; then
     echo "Error: Failed to register CECOM4CUT service to system."
     exit 1
 fi
+
+echo ""
+echo "Installed CECOM4CUT Service Daemon..."
+echo ""
+
+echo "Done..! CECOM4CUT Service has been installed Successfully."
