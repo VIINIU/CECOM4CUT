ffcff78d378494c8b5148da78da4985e4a7b07ff
yymin1022 <yymin1022@gmail.com>
2024-12-28 16:06:41 +0900

    Update cecom4cut_installer : Fixed file routes

--- cecom4cut_installer.sh
+++ cecom4cut_installer.sh
@@ -5,13 +5,13 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-bash /set_printer_mac.sh
+bash ./set_printer_mac.sh
 if [ $? -ne 0 ]; then
     echo "Error: Failed to find and register Printer."
     exit 1
 fi
 
-sh /Systemd_scripts/systemd_installer.sh
+sh ./Systemd_scripts/systemd_installer.sh
 if [ $? -ne 0 ]; then
     echo "Error: Failed to register CECOM4CUT service to system."
     exit 1
