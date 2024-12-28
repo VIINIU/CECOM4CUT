80e6207aeaf2b71130b76e997425a17a932e86bb
yymin1022 <yymin1022@gmail.com>
2024-12-28 15:59:59 +0900

    Update cecom4cut_installer : Added Exception for Dependency Installing

--- cecom4cut_installer.sh
+++ cecom4cut_installer.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 sudo apt update && sudo apt install -y python3 bluez
+if [ $? -ne 0 ]; then
+    echo "Error: Failed to install dependency packages."
+    exit 1
+fi
 
 bash /set_printer_mac.sh
 
