9521e91db3969637e8e4932b5af6550e999e7ca3
yymin1022 <yymin1022@gmail.com>
2025-03-11 11:09:53 +0000

    Update qr_generator : Adjust QR Canvas Size

--- Image_QR/qr_generator.py
+++ Image_QR/qr_generator.py
@@ -5,9 +5,7 @@ import sys
 def qr_gen(filename):
 	img_qr = qrcode.make(f"https://4cut.cecom.dev/download/{filename}")
 	img_qr = img_qr.resize((100, 100), Image.Resampling.LANCZOS)
-	img_bg = Image.new('RGB', (320, 100), 'white')
-	img_bg.paste(img_qr, (0, 0))
-	img_bg.save(f"{filename}.qr.bmp")
+	img_qr.save(f"{filename}.qr.bmp")
 
 def main(argv):
 	qr_gen(argv[1])
