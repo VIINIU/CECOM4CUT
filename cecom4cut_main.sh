63e579cee76b55bd7817745265b25854d70be058
yymin1022 <yymin1022@gmail.com>
2025-03-17 12:02:15 +0000

    Update cecom4cut_main : Refactored for changed frame

--- cecom4cut_main.sh
+++ cecom4cut_main.sh
@@ -2,12 +2,17 @@
 CURRENT_DATE=$(date +"%Y%m%d_%H%M%S")
 
 IMAGE_FILENAME=$CURRENT_DATE
-IMAGE_FRAME_HEADER_HEIGHT=35
-IMAGE_FRAME_FOOTER_HEIGHT=250
-IMAGE_PHOTO_HEIGHT=240
-IMAGE_WIDTH=320
+IMAGE_FRAME_HEADER_HEIGHT=85
+IMAGE_FRAME_FOOTER_HEIGHT=707
+IMAGE_PHOTO_HEIGHT=480
 
-QR_SIZE=100
+QR_SIZE=227
+
+IMAGE_HEIGHT=$((IMAGE_PHOTO_HEIGHT + IMAGE_FRAME_HEADER_HEIGHT + IMAGE_FRAME_FOOTER_HEIGHT + 30))
+IMAGE_WIDTH=640
+
+IMAGE_RESULT_HEIGHT=$((IMAGE_HEIGHT / 2))
+IMAGE_RESULT_WIDTH=$((IMAGE_WIDTH / 2))
 
 if [ -z "$PRINTER_MAC" ]; then
 	echo "Printer address is not registered."
@@ -24,16 +29,13 @@ python3 GPIO_Manager/gpio_led_camera_done.py
 echo "Capture Image Done..!"
 
 echo "Processing Image..."
-python3 Image_Processing/process.py "result/$IMAGE_FILENAME.jpg" "result/$IMAGE_FILENAME.bmp" $IMAGE_WIDTH $IMAGE_PHOTO_HEIGHT
-python3 Image_QR/qr_generator.py "result/$IMAGE_FILENAME"
-python3 Image_Processing/process.py "result/$IMAGE_FILENAME.qr.bmp" "result/$IMAGE_FILENAME.qr.bmp" $IMAGE_WIDTH $QR_SIZE
+python3 Image_QR/qr_generator.py "result/$IMAGE_FILENAME" $QR_SIZE
+python3 Image_Processing/merge.py "result/$IMAGE_FILENAME.jpg" "result/$IMAGE_FILENAME.jpg" "resources/frame_header.jpg" "resources/frame_footer.jpg" "result/$IMAGE_FILENAME.qr.bmp" $IMAGE_HEIGHT $IMAGE_WIDTH $IMAGE_PHOTO_HEIGHT $IMAGE_FRAME_HEADER_HEIGHT $IMAGE_FRAME_FOOTER_HEIGHT $QR_SIZE
+python3 Image_Processing/process.py "result/$IMAGE_FILENAME.jpg" "result/$IMAGE_FILENAME.bmp" $IMAGE_RESULT_WIDTH $IMAGE_RESULT_HEIGHT
 echo "Process Image Done..!"
 
 echo "Printing Image..."
-bash Printer_Scripts/print_bmp.sh "resources/frame_h_rev.bmp" $((IMAGE_FRAME_HEADER_HEIGHT + 5)) $IMAGE_WIDTH
-bash Printer_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.bmp" $((IMAGE_PHOTO_HEIGHT + 5)) $IMAGE_WIDTH
-bash Printer_Scripts/print_bmp.sh "resources/frame_f_rev.bmp" $((IMAGE_FRAME_FOOTER_HEIGHT + 5)) $IMAGE_WIDTH
-bash Printer_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.qr.bmp" $((QR_HEIGHT + 5)) $IMAGE_WIDTH
+bash Printer_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.bmp" $IMAGE_HEIGHT $IMAGE_WIDTH
 sleep 0.1
 bash Printer_Scripts/print_feed.sh 5
 echo "Print Image Done..!"
