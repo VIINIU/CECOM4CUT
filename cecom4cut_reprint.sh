7ea6f479c57c5a97a5428778576013d11bb3075b
yymin1022 <yymin1022@gmail.com>
2025-03-17 13:07:20 +0000

    Update cecom4cut_reprint : Refactored for changed frame

--- cecom4cut_reprint.sh
+++ cecom4cut_reprint.sh
@@ -2,10 +2,8 @@
 LAST_FILE_DATE=$(ls result/ | grep ".bmp" | grep -v "qr" | tac | head -n 1 | sed 's/\.bmp//')
 
 IMAGE_FILENAME=$LAST_FILE_DATE
-IMAGE_HEIGHT=240
+IMAGE_HEIGHT=636
 IMAGE_WIDTH=320
-QR_HEIGHT=100
-QR_WIDTH=320
 
 if [ -z "$PRINTER_MAC" ]; then
 	echo "Printer address is not registered."
@@ -16,13 +14,7 @@ fi
 sleep 1
 
 echo "Printing Image..."
-bash Printer_Scripts/print_bmp.sh "resources/frame_h_rev.bmp" 40 $IMAGE_WIDTH
-sleep 0.1
-bash Printer_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.bmp" $((IMAGE_HEIGHT + 5)) $IMAGE_WIDTH
-sleep 0.1
-bash Printer_Scripts/print_bmp.sh "resources/frame_f_rev.bmp" 255 $IMAGE_WIDTH
-sleep 0.1
-bash Printer_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.qr.bmp" $((QR_HEIGHT + 5)) $QR_WIDTH
+bash Printer_Scripts/print_bmp.sh "result/$IMAGE_FILENAME.bmp" $IMAGE_HEIGHT $IMAGE_WIDTH
 sleep 0.1
 bash Printer_Scripts/print_feed.sh 5
 echo "Print Image Done..!"
