0fd8c9262ba8c13feeca4edcc1652c184d639508
yymin1022 <yymin1022@gmail.com>
2025-03-17 11:57:18 +0000

    Update Image_QR : Added parameter for QR Size

--- Image_QR/qr_generator.py
+++ Image_QR/qr_generator.py
@@ -2,13 +2,13 @@ from PIL import Image
 import qrcode
 import sys
 
-def qr_gen(filename):
+def qr_gen(filename, qr_size):
 	img_qr = qrcode.make(f"https://4cut.cecom.dev/download/{filename}")
-	img_qr = img_qr.resize((100, 100), Image.Resampling.LANCZOS)
+	img_qr = img_qr.resize((qr_size, qr_size), Image.Resampling.LANCZOS)
 	img_qr.save(f"{filename}.qr.bmp")
 
 def main(argv):
-	qr_gen(argv[1])
+	qr_gen(argv[1], int(argv[2]))
 
 if __name__ == "__main__":
 	main(sys.argv)
